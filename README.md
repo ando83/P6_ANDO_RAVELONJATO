82e183cac8ab79abbc137ca25fc516867c1e948d
ando83 <ando.ravel@hotmail.com>
2021-02-11 16:12:21 +0100

    back end terminé, création route sauces

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
+*CONTEXTE DU PROJET
+-------------------
+So Pekocko est une entreprise familiale de 10 salariés. Son activité principale est la création de sauces piquantes dont la composition est tenue secrète. Forte de son succès, l’entreprise souhaite se développer et créer une application web, dans laquelle les utilisateurs pourront ajouter leurs sauces préférées et liker ou disliker les sauces proposées par les autres.
 
 
-PIQUANTE
+*TECHNOLOGIES UTILISÉES
+-----------------------
+● framework : Express ;
+● serveur : NodeJS ;
+● base de données : MongoDB ;
+● toutes les opérations de la base de données doivent utiliser le pack Mongoose avec
+des schémas de données stricts.
 
+* PIQUANTE
+----------
 Le projet a été généré avec Angular CLI version 7.0.2.
 
 Pour faire fonctionner le projet, vous devez installer node-sass à part.
 
-Development server
 
+* Development server
+--------------------
 Démarrer ng serve pour avoir accès au serveur de développement. Rendez-vous sur http://localhost:4200/. L'application va se recharger automatiquement si vous modifiez un fichier source.
+
--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,38 @@
-//importer express
+// importer express en local
 const express = require('express');
-
+// Importer le package bodyParser pour gérer la demande POST provenant de l'application front-end et pouvoir extraire l'objet JSON
+const bodyParser = require('body-parser');
+// Importer mongoose pour l'utilisation de la base de donnée
+const mongoose = require('mongoose');
+//nouvelle importation de node, donne accès au système de fichier
+const path = require('path');
+// Importer le router pour les sauces
+const sauceRoutes = require('./routes/sauce');
+// Importer le router pour les utilisateurs
+const userRoutes = require('./routes/user');
+//méthode pour se connecter à MongoDB
+mongoose.connect('mongodb+srv://andoravel1:evirananat00@cluster0.3onvn.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+// appliquer la méthode express
 const app = express();
-
-//fonctions middleware reçoivent les objets request, response et next
+// fonction(middleware) qui autorise à accéder aux ressources(api)
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    // rajoute header sur l'objet réponse
+    res.setHeader('Access-Control-Allow-Origin', '*'); //accessible pour toute origine 
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');//les headers qui sont autorisés à l'accès réponse
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');// les méthodes autorisées
     next();
   });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-  });
-
-//exporter app depuis les autres fichiers
+// Transformer le corps de la requête en objet javascript, json() est une méthode de l'objet bodyParser
+app.use(bodyParser.json());
+// Dédier à la partie du dossier des images
+app.use('/images', express.static(path.join(__dirname, 'images')));  
+// Routes pour les sauces
+app.use('/api/sauces', sauceRoutes);
+// Routes liées à l'authentification 
+app.use('/api/auth', userRoutes);
+// exporter app depuis les autres fichiers
 module.exports = app;
